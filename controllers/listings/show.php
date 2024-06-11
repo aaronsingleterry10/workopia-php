cb81bc704f01908e57d395f71416f07a01aae827
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-11 13:39:54 -0500

    added db instance with query, also get id

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -1,3 +1,15 @@
 <?php
+$config = require basePath('config/db.php');
+$db = new Database($config);
 
+// GET id from the query from URL
+$id = $_GET['id'] ?? '';
+
+$params = [
+    'id' => $id
+];
+
+$listing = $db->query('select * from listings where id = :id', $params)->fetch();
+
+inspect($listing);
 loadView('/listings/show');
