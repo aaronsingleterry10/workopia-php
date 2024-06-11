adee8c1ae13647c06fb01ee8e44effd07ec850ea
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-11 13:38:31 -0500

    modified query function to take in params and bind value

--- Database.php
+++ Database.php
@@ -31,9 +31,15 @@ class Database {
      * @thros PDOException
      */
 
-     public function query($query) {
+     public function query($query, $params = []) {
         try {
             $sth = $this->conn->prepare($query);
+
+            // Bind names params
+            foreach ($params as $param => $value) {
+                $sth->bindValue(':' . $param, $value);
+            }
+            
             $sth->execute();
             return $sth;
         } catch (PDOException $e) {
