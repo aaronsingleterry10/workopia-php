655223517cd8933a57b115b749d0f73a0f31249c
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-11 22:25:22 -0500

    created app folder and moved views and controllers in there

---
+++ App/controllers/listings/show.php
@@ -0,0 +1,16 @@
+<?php
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+// GET id from the query from URL
+$id = $_GET['id'] ?? '';
+
+$params = [
+    'id' => $id
+];
+
+$listing = $db->query('select * from listings where id = :id', $params)->fetch();
+
+loadView('/listings/show', [
+    'listing' => $listing
+]);
