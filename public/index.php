3d8cfb2ab37bdd7946c3083247e8c9005050b14a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-12 10:44:39 -0500

    added require for new vendor file

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
 require '../helpers.php';
 
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+
+// This eliminates the need for requires
+require __DIR__ . '/../vendor/autoload.php';
+
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 
 // Instantiate the router
