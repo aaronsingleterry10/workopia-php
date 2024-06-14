3f633ac78b4e4d67104aa7986f718b134eaa16d6
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-14 14:20:27 -0500

    added controller functionalities to HomeController

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -2,8 +2,24 @@
 
 namespace App\Controllers;
 
-class HomeController {
-    public function __construct() {
-        
+use Framework\Database;
+
+class HomeController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    public function index()
+    {
+        $listings = $this->db->query('select * from listings limit 6')->fetchAll();
+
+        loadView('home', [
+            'listings' => $listings
+        ]);
     }
 }
