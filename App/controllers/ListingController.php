ddcae7421b23b0f5fa85a5eca5e1ee60c13301bc
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-14 15:09:11 -0500

    added show function

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -4,6 +4,42 @@ namespace App\Controllers;
 
 use Framework\Database;
 
-class ListingController {
-    
+class ListingController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    public function index()
+    {
+
+        $listings = $this->db->query('select * from listings')->fetchAll();
+        loadView('listings/index', [
+            'listings' => $listings
+        ]);
+    }
+
+    public function create()
+    {
+        loadView('listings/create');
+    }
+
+    public function show()
+    {
+        $id = $_GET['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('select * from listings where id = :id', $params)->fetch();
+
+        loadView('/listings/show', [
+            'listing' => $listing
+        ]);
+    }
 }
