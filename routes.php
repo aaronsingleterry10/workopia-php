d40bb4194219364f235bc15e550f901377432408
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-17 20:03:45 -0500

    fixed error

--- routes.php
+++ routes.php
@@ -2,6 +2,6 @@
 
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
-$router->post('/listings', 'ListingController@store');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listing/{id}', 'ListingController@show');
+$router->post('/listings', 'ListingController@store');
