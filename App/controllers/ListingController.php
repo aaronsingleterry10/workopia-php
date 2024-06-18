e6bd609a7be8c0c31c755becb2b9ecd4df54139c
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-17 20:56:58 -0500

    added store function to add new job listing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -80,6 +80,8 @@ class ListingController
 
         $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
 
+        $newListingData['user_id'] = 1;
+
         $newListingData = array_map('sanitize', $newListingData);
 
         $requiredFields = ['title', 'description', 'email', 'city', 'state'];
@@ -98,7 +100,31 @@ class ListingController
                 'listing' => $newListingData
             ]);
         } else {
-            echo 'Submitted';
+            $fields = [];
+
+            foreach ($newListingData as $field => $value) {
+                $fields[] = $field;
+            }
+
+            $fields = implode(', ', $fields);
+
+            $values = [];
+
+            foreach ($newListingData as $field => $value) {
+                // Convert empty strings to null
+                if ($value === '') {
+                    $newListingData[$field] = null;
+                }
+                $values[] = ':' . $field;
+            }
+
+            $values = implode(', ', $values);
+
+            $query = "insert into listings ({$fields}) values ({$values})";
+
+            $this->db->query($query, $newListingData);
+
+            redirect('/listings');
         }
     }
 }
