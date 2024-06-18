cce405d08fe59558918e3ed0b0c9d50701994887
Aaron Singleterry <aaronsingleterry10@gmail.com>
2024-06-17 21:24:32 -0500

    added conditional for tags

--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -13,7 +13,7 @@
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <!-- Job Listings -->
-            <?php foreach($listings as $value) :?>
+            <?php foreach ($listings as $value): ?>
                 <div class="rounded-lg shadow-md bg-white">
                     <div class="p-4">
                         <h2 class="text-xl font-semibold"><?= $value->title ?></h2>
@@ -26,10 +26,11 @@
                                 <strong>Location:</strong> <?= $value->city ?>, <?= $value->state ?>
                                 <span class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span>
                             </li>
-                            <li class="mb-2">
-                                <strong>Tags:</strong> <span><?= $value->tags ?></span>,
-                                <span>Coding</span>
-                            </li>
+                            <?php if (!empty($value->tags)): ?>
+                                <li class="mb-2">
+                                    <strong>Tags:</strong> <span><?= $value->tags ?></span>
+                                </li>
+                            <?php endif ?>
                         </ul>
                         <a href="/listing/<?= $value->id ?>"
                             class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
